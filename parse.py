b97d08ef301f945016fa74a39b24baf7d2c6646a
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-11-30 14:13:30 +0200

    finally figured out what was necessary

--- parse.py
+++ parse.py
@@ -11,7 +11,33 @@ def dump_dict_to_json(data):
     return json.dumps(data, sort_keys=True, indent=4)
 
 
-def extract_required_data(data):
-    return data['KPT']['CadastralBlocks']['CadastralBlock']['ObjectsRealty']['ObjectRealty']     
+def get_raw_parcels(data):
+    return [parcel for parcel in data['KPT']['CadastralBlocks']['CadastralBlock']['Parcels']['Parcel']
+        if 'EntitySpatial' in parcel
+    ]
 
-print dump_dict_to_json(extract_required_data(parse_to_dict(sample_file_path)))
+
+def clean_parcels(raw_parcels):
+    return [{
+        'cadastral_number': parcel['@CadastralNumber'],
+        'path': [{
+                'su_nmb': point['@SuNmb'],
+                'x': point['ns3:Ordinate']['@X'],
+                'y': point['ns3:Ordinate']['@Y']
+            } for point in parcel['EntitySpatial']['ns3:SpatialElement']['ns3:SpelementUnit']]
+        } for parcel in raw_parcels
+        if 'ns3:SpatialElement' in parcel['EntitySpatial']
+        and 'ns3:SpelementUnit' in parcel['EntitySpatial']['ns3:SpatialElement']
+    ]
+
+
+def store_as_csv(parcels, output_file_path):
+    import csv
+    with open(output_file_path, 'wb') as csvfile:
+        csv_writer = csv.writer(csvfile)#, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(['cadastral_number', 'su_nmb', 'x', 'y'])
+        for parcel in parcels:
+            for point in parcel['path']:
+                csv_writer.writerow([parcel['cadastral_number'], point['su_nmb'], point['x'], point['y']])
+
+store_as_csv(clean_parcels(get_raw_parcels(parse_to_dict(sample_file_path))), './parcels.csv')
