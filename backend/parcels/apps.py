ed42820daa42577925b97cf01a057821c97baedc
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-12-02 20:41:23 +0200

    cleaned gh-pages

--- backend/parcels/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-from django.utils.translation import ugettext_lazy as _
-
-
-class ParcelsConfig(AppConfig):
-    name = 'parcels'
-    verbose_name = _("Parcels")
--- backend/parcels/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-
-class CadastralBlockFileUploadForm(forms.Form):
-    files = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
