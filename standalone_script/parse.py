a6a7ba310c3ca12dff14e405f55f742df3897c49
Grigoriy Beziuk <gbezyuk@gmail.com>
2016-12-02 16:10:41 +0200

    updated standalone script

--- standalone_script/parse.py
+++ standalone_script/parse.py
@@ -1,9 +1,8 @@
-sample_file_path = './data-samples/doc14350111.xml'
-
+#!/usr/bin/env python3
 
 def parse_to_dict(file_path):
     import xmltodict
-    return xmltodict.parse(file(file_path).read())    
+    return xmltodict.parse(open(file_path).read())    
 
 
 def dump_dict_to_json(data):
@@ -33,11 +32,55 @@ def clean_parcels(raw_parcels):
 
 def store_as_csv(parcels, output_file_path):
     import csv
-    with open(output_file_path, 'wb') as csvfile:
+    with open(output_file_path, 'wt') as csvfile:
         csv_writer = csv.writer(csvfile)#, delimiter=' ', quotechar='|', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow(['cadastral_number', 'su_nmb', 'x', 'y'])
         for parcel in parcels:
             for point in parcel['path']:
                 csv_writer.writerow([parcel['cadastral_number'], point['su_nmb'], point['x'], point['y']])
 
-store_as_csv(clean_parcels(get_raw_parcels(parse_to_dict(sample_file_path))), './parcels.csv')
+
+def get_parcels(input_file_path):
+    return clean_parcels(get_raw_parcels(parse_to_dict(input_file_path))),
+
+# sample_input_path = './data-samples/doc14350111.xml'
+# sample_output_path = './parcels.csv'
+
+# store_as_csv(get_parcels(input_path), output_path)
+
+
+if __name__ == '__main__':
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file_path", help="input xml file to parse", type=str)
+    parser.add_argument("-c", "--csv_output_file_path", help="output csv file to export data", type=str)
+    parser.add_argument("-j", "--json_output_file_path", help="output csv file to export data", type=str)
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    args = parser.parse_args()
+
+    if args.verbose:
+        print("Parsing original document...")
+    document_as_dict = parse_to_dict(args.input_file_path)
+    if args.verbose:
+        print("Parsing original document DONE")
+
+    if args.json_output_file_path:
+        if args.verbose:
+            print("Storing JSON file...")
+        with open(args.json_output_file_path, 'wt') as jsonfile:
+            jsonfile.write(dump_dict_to_json(document_as_dict))
+        if args.verbose:
+            print("Storing JSON file DONE")
+
+    if args.csv_output_file_path:
+        if args.verbose:
+            print("Extracting Parcels...")
+        parcels = clean_parcels(get_raw_parcels(document_as_dict))
+        if args.verbose:
+            print("Extracting Parcels DONE")
+
+        if args.verbose:
+            print("Storing CSV file...")
+        store_as_csv(parcels, args.csv_output_file_path)
+        if args.verbose:
+            print("Storing CSV file DONE")
